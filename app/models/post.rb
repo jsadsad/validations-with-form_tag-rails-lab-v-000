e03eaf6a324c18b9fde6d4dbc407f6ab38bca8db
Joshua Clarence Sadsad <joshsad2@gmail.com>
2019-08-13 18:36:20 +0000

    Automatically backed up by Learn

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,5 @@
 class Post < ActiveRecord::Base
   validates :title, presence: true
   validates :category, length: {minimum: 50}
-  validates :content, inclusion: {in: %w(Fiction Non Fiction)}
+  validates :content, inclusion: {in: %w(Fiction Non-Fiction)}
 end
