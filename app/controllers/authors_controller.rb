bceda3d1b01a6d5bbe448e9d573ffc6e40ab5a90
Joshua Clarence Sadsad <joshsad2@gmail.com>
2019-08-13 18:30:20 +0000

    Automatically backed up by Learn

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -9,12 +9,16 @@ class AuthorsController < ApplicationController
 
   def create
     @author = Author.new(author_params)
+
     if @author.save
       redirect_to author_path(@author)
     else
       render :new
     end
   end
+
+  def update
+    
 
   private
 
